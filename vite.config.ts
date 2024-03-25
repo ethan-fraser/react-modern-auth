78374b330b94877b4e875fd5922061c1a72c53fc
Ethan Fraser <ejpfraser@gmail.com>
2024-03-25 15:50:00 +1300

    update build process for lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: ["src"] }), libInjectCss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
